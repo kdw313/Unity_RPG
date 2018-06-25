b9251106c152b1ca2e9191a1ba66a556df5aa1f2
Shawn Kim <sdf1342@gmail.com>
2018-06-24 22:54:59 -0700

    #init

---
+++ Golem And Pumpkins/Assets/Scripts/PumpkinScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PumpkinScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Update () {
+        if (transform.position.y < -5f)
+            Destroy(gameObject);
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Golem")
+        {
+            Destroy(collision.gameObject);
+            Time.timeScale = 0f;
+        }
+        
+    }
+}
